1a458e20f2541304a817491d15df0a95c0f38f7a
FS <f.scholze@gmx.de>
2025-05-21 10:19:19 +0200

    Handle german language too

--- backend/app.py
+++ backend/app.py
@@ -266,20 +266,20 @@ def main():
 
         # Language-specific text processing
         if language == "hsb":
-            print("processing for: hsb")
+            logger.debug("processing for: hsb")
 
             for match in re.findall(r"\b\d{4}-\d{2,4}\b", text):
                 first_num, sec_num = match.split("-")
-                first_num_txt = year_to_text(first_num)
+                first_num_txt = year_to_text(first_num, language)
                 sec_num_txt = year_to_text(sec_num)
                 text = text.replace(match, f"{first_num_txt} do {sec_num_txt}")
 
             for match in re.findall(r"\d{1,2}:\d{2}\s*hodź(?:\.|in)?", text):
                 first_num, sec_num = match.split(":")
                 sec_num = "".join(char for char in sec_num if char.isdigit())
-                first_num_txt = number_to_text(first_num)
+                first_num_txt = number_to_text(first_num, language)
                 if sec_num != "00":
-                    sec_num_txt = number_to_text(sec_num)
+                    sec_num_txt = number_to_text(sec_num, language)
                 else:
                     sec_num_txt = ""
                 text = text.replace(match, f"{first_num_txt} hodźin {sec_num_txt}")
@@ -317,7 +317,7 @@ def main():
                         abbr_start = None
                     elif laststate == "num":
                         num = text[num_start:index]
-                        res_text = res_text + " " + number_to_text(num) + " "
+                        res_text = res_text + " " + number_to_text(num, language) + " "
                         num_start = None
 
                 if curstate == "char":
@@ -331,11 +331,73 @@ def main():
             res_text = res_text.replace("\xad", "")
             res_text = res_text.replace("x", "ks")
         elif language == "de":
-          print("processing for: de")
-            # TODO: Add language-specific text processing for german languages
+          logger.debug("processing for: de")
+
+          for match in re.findall(r"\b\d{4}-\d{2,4}\b", text):
+              first_num, sec_num = match.split("-")
+              first_num_txt = year_to_text(first_num, language)
+              sec_num_txt = year_to_text(sec_num, language)
+              text = text.replace(match, f"{first_num_txt} bis {sec_num_txt}")
+
+          for match in re.findall(r"\d{1,2}:\d{2}\s*Uhr?", text):
+              first_num, sec_num = match.split(":")
+              sec_num = "".join(char for char in sec_num if char.isdigit())
+              first_num_txt = number_to_text(first_num, language)
+              if sec_num != "00":
+                  sec_num_txt = number_to_text(sec_num, language)
+              else:
+                  sec_num_txt = ""
+              text = text.replace(match, f"{first_num_txt} Uhr {sec_num_txt}")
+
+          abbr_start = None
+          num_start = None
+          res_text = ""
+          curstate = "char"
+          laststate = ""
+          for index in range(len(text)):
+              char = text[index]
+              if char.isupper():
+                  if abbr_start is None:
+                      abbr_start = index
+                      curstate = "abbr"
+              elif is_number(char):
+                  if num_start is None:
+                      num_start = index
+                      curstate = "num"
+              else:
+                  curstate = "char"
+
+              if curstate != laststate:
+                  if laststate == "abbr":
+                      written_abbr = ""
+                      abbr = text[abbr_start:index]
+                      if len(abbr) > 1:
+                          for letter in abbr:
+                              written_abbr = (
+                                  f"{written_abbr} {char_to_spoken[letter.lower()]}"
+                              )
+                          res_text = res_text + " " + written_abbr + " "
+                      else:
+                          res_text = res_text + abbr
+                      abbr_start = None
+                  elif laststate == "num":
+                      num = text[num_start:index]
+                      res_text = res_text + " " + number_to_text(num, language) + " "
+                      num_start = None
+
+              if curstate == "char":
+                  res_text = res_text + char
+
+              laststate = curstate
+
+          if speaker_config[speaker_id]["lower"]:
+              res_text = res_text.lower()
+          res_text = res_text.replace("  ", " ")
+          res_text = res_text.replace("\xad", "")
+      
         else:
             # TODO: Add language-specific text processing for other languages
-            print("processing for: unknown language")
+            logger.debug("processing for: unknown language")
             res_text = text
   
         temp_wav_file_path = f"temp/{uuid.uuid4().hex}.wav"
--- backend/utils.py
+++ backend/utils.py
@@ -1,221 +1,157 @@
 import math
+from number_words import number_words
 
-numberWord0To9 = {
-    0: "nul",
-    1: "jedyn",
-    2: "dwaj",
-    3: "tři",
-    4: "štyri",
-    5: "pjeć",
-    6: "šěsć",
-    7: "sydom",
-    8: "wosom",
-    9: "dźewjeć",
-}
+def get_number_words(lang):
+    """Get number words dictionary for specified language."""
+    if lang not in number_words:
+        raise ValueError(f"Language {lang} not supported!")
+    return number_words[lang]
 
-numberWord10To19 = {
-    10: "dźesać",
-    11: "jědnaće",
-    12: "dwanaće",
-    13: "třinaće",
-    14: "štyrnaće",
-    15: "pjatnaće",
-    16: "šěsnaće",
-    17: "sydomnaće",
-    18: "wosomnaće",
-    19: "dźewjatnaće",
-}
-
-numberWord20To90 = {
-    2: "dwaceći",
-    3: "třiceći",
-    4: "štyrceći",
-    5: "pjećdźesat",
-    6: "šěsćdźesat",
-    7: "sydomdźesat",
-    8: "wosomdźesat",
-    9: "dźewjećdźesat",
-}
-
-numberWord100To900 = {
-    1: "sto",
-    2: "dwěsćě",
-    3: "třista",
-    4: "štyrista",
-    5: "pjećstow",
-    6: "šěsćstow",
-    7: "sydomstow",
-    8: "wosomstow",
-    9: "dźewjećstow",
-}
-
-numberWord1000plus = {
-    1e3: "tysac",
-    1e6: "jedyn milion",  # milion maskulinum
-    2e6: "dwaj milionaj",
-    3e6: "tři miliony",
-    4e6: "štyri miliony",
-    5e6: "pjeć milionow",
-    "6e6+": "milionow",
-    1e9: "jedna miliarda",  # miliarda femininum
-    2e9: "dwě miliardźe",
-    3e9: "tři miliardy",
-    4e9: "štyri miliardy",
-    5e9: "pjeć miliardow",
-    "6e9+": "miliardow",
-    1e12: "jedyn bilion",  # bilion maskulinum
-    2e12: "dwaj bilionaj",
-    3e12: "tři biliony",
-    4e12: "štyri biliony",
-    5e12: "pjeć bilionow",
-    "6e12+": "bilionow",
-}
-
-
-def spellNumber0to99(num):
-    if num < 0 and num > 100:
+def spellNumber0to99(num, lang):
+    if num < 0 or num > 99:
         raise ValueError("number out of range!")
 
+    words = get_number_words(lang)
+    
     if num < 10:
-        return numberWord0To9[num]
+        return words["0to9"][num]
     elif num >= 10 and num < 20:
-        return numberWord10To19[num]
+        return words["10to19"][num]
     elif num >= 20 and num < 100:
         num1 = math.floor(num / 10)
         num2 = num % 10
         if num2 == 0:
-            return numberWord20To90[num1]
+            return words["20to90"][num1]
         else:
-            return f"{numberWord0To9[num2]} a {numberWord20To90[num1]}"
+            return f"{words['0to9'][num2]} {lang == 'hsb' and 'a' or 'und'} {words['20to90'][num1]}"
 
-
-def spellNumber100to999(num):
-    if num < 100 and num > 1000:
+def spellNumber100to999(num, lang):
+    if num < 100 or num > 999:
         raise ValueError("number out of range!")
 
+    words = get_number_words(lang)
     num1 = math.floor(num / 100)
     num2 = num % 100
 
     if num2 == 0:
-        return f"{numberWord100To900[num1]}"
+        return words["100to900"][num1]
     else:
-        return f"{numberWord100To900[num1]} {spellNumber0to99(num2)}"
+        return f"{words['100to900'][num1]} {spellNumber0to99(num2, lang)}"
 
+def spellNumber0to999(num, lang):
+    if num < 0 or num > 999:
+        raise ValueError("number out of range!")
 
-def spellNumber0to999(num):
+    if num < 100:
+        return spellNumber0to99(num, lang)
+    else:
+        return spellNumber100to999(num, lang)
 
-    if num >= 0 and num < 100:
-        return spellNumber0to99(num)
-    elif num >= 100 and num < 1000:
-        return spellNumber100to999(num)
-    return ValueError("number out of range!")
-
-
-def spellNumber1000to999999(num):
-
+def spellNumber1000to999999(num, lang):
+    words = get_number_words(lang)
     texts = []
 
     num1 = math.floor(num / 1e3)
     num2 = num % 1e3
 
     if num1 > 0:
-        if num != 1:
-            texts.append(spellNumber0to999(num1))
-        texts.append(numberWord1000plus[1e3])
+        if num1 != 1:
+            texts.append(spellNumber0to999(num1, lang))
+        texts.append(words["1000plus"][1e3])
     if num2 > 0:
-        texts.append(spellNumber0to999(num2))
+        texts.append(spellNumber0to999(num2, lang))
     return " ".join(texts)
 
-
-def spellNumberMil(num):
+def spellNumberMil(num, lang):
+    words = get_number_words(lang)
     texts = []
     num1 = math.floor(num / 1e6)
     num2 = num % 1e6
 
     if num1 > 0:
         if num1 == 1:
-            texts.append(numberWord1000plus[1e6])
+            texts.append(words["1000plus"][1e6])
         elif num1 == 2:
-            texts.append(numberWord1000plus[2e6])
+            texts.append(words["1000plus"][2e6])
         elif num1 == 3:
-            texts.append(numberWord1000plus[3e6])
+            texts.append(words["1000plus"][3e6])
         elif num1 == 4:
-            texts.append(numberWord1000plus[4e6])
+            texts.append(words["1000plus"][4e6])
         elif num1 == 5:
-            texts.append(numberWord1000plus[5e6])
+            texts.append(words["1000plus"][5e6])
         else:
-            texts.append(spellNumber0to999(num1))
-            texts.append(numberWord1000plus["6e6+"])
+            texts.append(spellNumber0to999(num1, lang))
+            texts.append(words["1000plus"]["6e6+"])
 
     if num2 > 0:
-        texts.append(spellNumber1000to999999(num2))
+        texts.append(spellNumber1000to999999(num2, lang))
     return " ".join(texts)
 
-
-def spellNumberMrd(num):
+def spellNumberMrd(num, lang):
+    words = get_number_words(lang)
     texts = []
     num1 = math.floor(num / 1e9)
     num2 = num % 1e9
 
     if num1 > 0:
         if num1 == 1:
-            texts.append(numberWord1000plus[1e9])
+            texts.append(words["1000plus"][1e9])
         elif num1 == 2:
-            texts.append(numberWord1000plus[2e9])
+            texts.append(words["1000plus"][2e9])
         elif num1 == 3:
-            texts.append(numberWord1000plus[3e9])
+            texts.append(words["1000plus"][3e9])
         elif num1 == 4:
-            texts.append(numberWord1000plus[4e9])
+            texts.append(words["1000plus"][4e9])
         elif num1 == 5:
-            texts.append(numberWord1000plus[5e9])
+            texts.append(words["1000plus"][5e9])
         else:
-            texts.append(spellNumber0to999(num1))
-            texts.append(numberWord1000plus["6e9+"])
+            texts.append(spellNumber0to999(num1, lang))
+            texts.append(words["1000plus"]["6e9+"])
 
     if num2 > 0:
-        texts.append(spellNumberMil(num2))
+        texts.append(spellNumberMil(num2, lang))
     return " ".join(texts)
 
-
-def spellNumberBil(num):
+def spellNumberBil(num, lang):
+    words = get_number_words(lang)
     texts = []
     num1 = math.floor(num / 1e12)
     num2 = num % 1e12
 
     if num1 > 0:
         if num1 == 1:
-            texts.append(numberWord1000plus[1e12])
+            texts.append(words["1000plus"][1e12])
         elif num1 == 2:
-            texts.append(numberWord1000plus[2e12])
+            texts.append(words["1000plus"][2e12])
         elif num1 == 3:
-            texts.append(numberWord1000plus[3e12])
+            texts.append(words["1000plus"][3e12])
         elif num1 == 4:
-            texts.append(numberWord1000plus[4e12])
+            texts.append(words["1000plus"][4e12])
         elif num1 == 5:
-            texts.append(numberWord1000plus[5e12])
+            texts.append(words["1000plus"][5e12])
         else:
-            texts.append(spellNumber0to999(num1))
-            texts.append(numberWord1000plus["6e12+"])
+            texts.append(spellNumber0to999(num1, lang))
+            texts.append(words["1000plus"]["6e12+"])
 
     if num2 > 0:
-        texts.append(spellNumberMrd(num2))
+        texts.append(spellNumberMrd(num2, lang))
     return " ".join(texts)
 
-
-def year_to_text(num):
+def year_to_text(num, lang):
     num = int(num)
+    if lang not in ["hsb", "de"]:
+        raise ValueError("language not supported!")
+    
     if num > 1099 and num < 2000:
         num1 = math.floor(num / 100)
-        num1_txt = spellNumber0to99(num1)
+        num1_txt = spellNumber0to99(num1, lang)
         num2 = num % 100
-        num2_txt = spellNumber0to99(num2)
-        return f"{num1_txt} stow {num2_txt}"
+        num2_txt = spellNumber0to99(num2, lang)
+        return f"{num1_txt} stow {num2_txt}" if lang == "hsb" else f"{num1_txt} hundert {num2_txt}"
     else:
-        return number_to_text(num)
+        return number_to_text(num, lang)
 
-
-def number_to_text(num):
-
+def number_to_text(num, lang):
     try:
         int(num)
     except ValueError:
@@ -224,14 +160,14 @@ def number_to_text(num):
     num = int(num)
 
     if num >= 0 and num < 1000:
-        return spellNumber0to999(num)
+        return spellNumber0to999(num, lang)
     elif num >= 1e3 and num < 1e6:
-        return spellNumber1000to999999(num)
+        return spellNumber1000to999999(num, lang)
     elif num >= 1e6 and num < 1e9:
-        return spellNumberMil(num)
+        return spellNumberMil(num, lang)
     elif num >= 1e9 and num < 1e12:
-        return spellNumberMrd(num)
+        return spellNumberMrd(num, lang)
     elif num >= 1e12 and num < 1e15:
-        return spellNumberBil(num)
+        return spellNumberBil(num, lang)
     else:
         raise ValueError("number out of range!")
