f26e98d26997a1a7461d94205b47063fb5b55d9a
Daniel Zoba <d.zoba@zalozba.de>
2025-10-27 13:00:45 +0100

    enable all announced timbres

--- backend/voicechanger.py
+++ backend/voicechanger.py
@@ -5,6 +5,8 @@ from openvoice.api import ToneColorConverter
 import sys
 
 def change_voice(wavfilename, speaker_id, timbre_id, emotion, model, logger):
+    
+    # have this the only supported changer model for now
     ckpt_converter = "voice_changer/models/checkpoints_v2/converter"
     device="cuda:0" if torch.cuda.is_available() else "cpu"
     
@@ -14,7 +16,13 @@ def change_voice(wavfilename, speaker_id, timbre_id, emotion, model, logger):
     base_speaker = wavfilename
     source_se, audio_name = se_extractor.get_se(base_speaker, tone_color_converter, vad=True)
 
-    reference_speaker = "voice_changer/katka/katka_" + emotion + ".wav"
+    # handle all special cases for emotions
+    if timbre_id == "katka_2025_07":
+    	timbre_id = "katka"
+    if timbre_id == "michal_multi_2025_02_20":
+    	timbre_id = "michal"
+
+    reference_speaker = "voice_changer/" + timbre_id + "/" + timbre_id + "_" + emotion + ".wav"
     target_se, audio_name = se_extractor.get_se(reference_speaker, tone_color_converter, vad=True)
 
     save_path = wavfilename + ".changed.wav"
