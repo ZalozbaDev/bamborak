c9070c489b5505c15232564ebbd81616805dabf6
Daniel Zoba <danielzoba@Mac.fritz.box>
2025-12-03 10:21:07 +0100

    use python 3.11 for bamborak / older coqui

--- backend/0001_venv.sh
+++ backend/0001_venv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 -m venv pythonenv/
+python3.11 -m venv pythonenv/
--- backend/0005_install.sh
+++ backend/0005_install.sh
@@ -2,7 +2,7 @@
 
 source pythonenv/bin/activate
 
-pip3 install coqui-tts
+pip3.11 install -r requirements_pinned_macos.txt
 
 cp ../../VITS/config.json     ./thorsten.json
 cp ../../VITS/model_file.pth  ./thorsten.pth
