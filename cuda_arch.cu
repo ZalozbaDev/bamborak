415dd7211af1a706e8e9abeb71b22a31a417a4a5
Daniel Zoba <d.zoba@zalozba.de>
2026-08-06 16:18:45 +0200

    script that assembles voice_changer directory

--- cuda_arch.cu
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cuda_runtime.h>
-
-int main() {
-    cudaDeviceProp prop;
-    cudaGetDeviceProperties(&prop, 0);
-    printf("architecture: %d%d\n", prop.major, prop.minor);
-    return 0;
-}
