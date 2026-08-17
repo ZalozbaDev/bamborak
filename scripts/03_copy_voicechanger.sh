c69819896549a0dfffa4df4a0885237cdbaa6774
Daniel Zoba <d.zoba@zalozba.de>
2026-08-17 09:31:31 +0200

    clone from public URL not private one

--- scripts/03_copy_voicechanger.sh
+++ scripts/03_copy_voicechanger.sh
@@ -6,7 +6,7 @@ cp -r voice_chager/* backend_data/voice_changer/
 
 # copy voicechanger models
 if ! [ -e tmp_openvoice ]; then
-	git clone git@github.com:ZalozbaDev/OpenVoice tmp_openvoice
+	git clone https://github.com/ZalozbaDev/OpenVoice.git tmp_openvoice
 fi
 pushd tmp_openvoice
 cat checkpoints_v2_0417.zip0? > checkpoints_v2_0417.zip
