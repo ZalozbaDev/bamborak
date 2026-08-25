fb19d0258f3f9fff9db37117c2b7a8136ed95150
Daniel Zoba <d.zoba@zalozba.de>
2026-08-25 11:27:32 +0200

    changes for running in unprivileged frontend container

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cat <<EOF > /usr/share/nginx/html/config.js
+cat <<EOF > /usr/share/nginx/html/config/runtime-config.js
 window.RUNTIME_CONFIG = {
   VITE_URL: "${VITE_URL}",
   VITE_URL_RAG: "${VITE_URL_RAG}"
